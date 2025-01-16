791c104696190e86daf3c8295efb931171edebc0
Nuutti Niemelä <128821034+NuuNiem@users.noreply.github.com>
2025-01-16 19:03:18 +0200

    removed toolbar

--- src/dashboard.py
+++ src/dashboard.py
@@ -167,4 +167,4 @@ def update_map(min_distance):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run_server(debug=False, host="0.0.0.0", port=port)
